a50c5f2c025dd45ae702ff7a92f0e38644e0fc5e
Korey Peters <72994607+koreypeters-dexcom@users.noreply.github.com>
2026-05-25 13:45:17 -0600

    Test speedups

--- hf/settings/dev.py
+++ hf/settings/dev.py
@@ -5,3 +5,5 @@ DEBUG = True
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "localhost"
 EMAIL_PORT = 1025
+
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
