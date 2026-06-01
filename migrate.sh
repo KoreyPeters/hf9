23b3ad81f2917c9ea867e40c4a4ae4a32b5c57eb
Korey Peters <me@koreypeters.org>
2026-06-01 11:18:25 -0600

    Terraform fixes

--- migrate.sh
+++ migrate.sh
@@ -8,7 +8,3 @@ litestream restore \
   "gcs://${LITESTREAM_GCS_BUCKET}/hf/db"
 
 python manage.py collectstatic --noinput
-
-exec litestream replicate \
-  -config /app/litestream.yml \
-  -exec "python manage.py migrate --noinput"
--- start.sh
+++ start.sh
@@ -7,6 +7,8 @@ litestream restore \
   -o /data/db.sqlite3 \
   "gcs://${LITESTREAM_GCS_BUCKET}/hf/db"
 
+python manage.py migrate --noinput
+
 exec litestream replicate \
   -config /app/litestream.yml \
   -exec "uvicorn hf.asgi:application --host 0.0.0.0 --port 8080 --workers 4"
